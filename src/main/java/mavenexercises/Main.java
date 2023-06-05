09c6b3b136b1c7ba59a85a79caf5529d9aa09e44
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-06-05 15:01:43 -0600

    maven exercises

---
+++ src/main/java/mavenexercises/Main.java
@@ -0,0 +1,24 @@
+package mavenexercises;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Something");
+        String userInput = sc.next();
+
+        boolean isNumeric = StringUtils.isNumeric(userInput);
+        System.out.println("Is Numeric: " + isNumeric);
+
+        String swapCase = StringUtils.swapCase(userInput);
+        System.out.println("Swap case: " + swapCase);
+
+        String reverseString = StringUtils.reverse(userInput);
+        System.out.println("Reverse String: " + reverseString);
+
+    }
+}
